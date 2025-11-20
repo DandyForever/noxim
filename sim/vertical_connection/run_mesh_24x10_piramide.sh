95e201c4f01e0da71e98d561c9e410ba737c409a
dandy4ever31 <dandy4ever31@gmail.com>
2025-11-20 19:19:09 +0300

    actualized vertical connection config + added validation script

---
+++ sim/vertical_connection/run_mesh_24x10_piramide.sh
@@ -0,0 +1,14 @@
+../../build/noxim -config ../../config_examples/vertical_connection/mesh_24x10_piramide.yaml -pir 0.05 poisson -sim 10000 -detailed -log_file_name ../results/vertical_connection/piramide
+../../build/noxim -config ../../config_examples/vertical_connection/mesh_24x10_piramide.yaml -pir 0.10 poisson -sim 10000 -detailed -log_file_name ../results/vertical_connection/piramide
+../../build/noxim -config ../../config_examples/vertical_connection/mesh_24x10_piramide.yaml -pir 0.15 poisson -sim 10000 -detailed -log_file_name ../results/vertical_connection/piramide
+../../build/noxim -config ../../config_examples/vertical_connection/mesh_24x10_piramide.yaml -pir 0.20 poisson -sim 10000 -detailed -log_file_name ../results/vertical_connection/piramide
+../../build/noxim -config ../../config_examples/vertical_connection/mesh_24x10_piramide.yaml -pir 0.25 poisson -sim 10000 -detailed -log_file_name ../results/vertical_connection/piramide
+../../build/noxim -config ../../config_examples/vertical_connection/mesh_24x10_piramide.yaml -pir 0.30 poisson -sim 10000 -detailed -log_file_name ../results/vertical_connection/piramide
+../../build/noxim -config ../../config_examples/vertical_connection/mesh_24x10_piramide.yaml -pir 0.35 poisson -sim 10000 -detailed -log_file_name ../results/vertical_connection/piramide
+../../build/noxim -config ../../config_examples/vertical_connection/mesh_24x10_piramide.yaml -pir 0.40 poisson -sim 10000 -detailed -log_file_name ../results/vertical_connection/piramide
+../../build/noxim -config ../../config_examples/vertical_connection/mesh_24x10_piramide.yaml -pir 0.50 poisson -sim 10000 -detailed -log_file_name ../results/vertical_connection/piramide
+../../build/noxim -config ../../config_examples/vertical_connection/mesh_24x10_piramide.yaml -pir 0.60 poisson -sim 10000 -detailed -log_file_name ../results/vertical_connection/piramide
+../../build/noxim -config ../../config_examples/vertical_connection/mesh_24x10_piramide.yaml -pir 0.70 poisson -sim 10000 -detailed -log_file_name ../results/vertical_connection/piramide
+../../build/noxim -config ../../config_examples/vertical_connection/mesh_24x10_piramide.yaml -pir 0.80 poisson -sim 10000 -detailed -log_file_name ../results/vertical_connection/piramide
+../../build/noxim -config ../../config_examples/vertical_connection/mesh_24x10_piramide.yaml -pir 0.90 poisson -sim 10000 -detailed -log_file_name ../results/vertical_connection/piramide
+../../build/noxim -config ../../config_examples/vertical_connection/mesh_24x10_piramide.yaml -pir 1.00 poisson -sim 10000 -detailed -log_file_name ../results/vertical_connection/piramide
